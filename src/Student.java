e0949893f623e95ea934f17e75dc0e5f94986b22
kuroame13013 <kuroame13013@gmail.com>
2021-07-03 23:52:45 +0900

    [add]maxScoreを追加

--- src/Student.java
+++ src/Student.java
@@ -10,11 +10,17 @@ public class Student {
     void setName(String newName){
         name = newName;
     }
-    void printScore(){
+    void printScore(int maxScore){
+        System.out.println("今回テストの満点は" + maxScore + "点です！");
         System.out.println(name + "さんの点数は" + score + "点です！！");
     }
 
+    void printScore(){
+        printScore(MAX_SCORE);
+    }
+
     int getScore(){
+
         return score;
     }
 
